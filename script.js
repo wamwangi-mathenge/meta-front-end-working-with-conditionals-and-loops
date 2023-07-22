c95e35380ff95971ba429a01b3d219d563c30a95
Brian <wamwangimathenge@gmail.com>
2023-07-22 14:23:17 +0300

    Solve Exercise 1

---
+++ script.js
@@ -0,0 +1,14 @@
+// Exercise 1
+
+for(i=1; i<=10; i++) {
+    // console.log(i)
+    if(i==1) {
+        console.log("Gold Medal")
+    } else if(i==2) {
+        console.log("Silver Medal")
+    } else if(i==3) {
+        console.log("Bronze Medal")
+    } else {
+        console.log(i)
+    }
+}
